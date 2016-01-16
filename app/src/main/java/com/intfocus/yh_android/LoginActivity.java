8ef8930a964f815d7031895e4047040b27fc900d
jay16 <jay_li@intfocus.com>
2016-01-16 17:58:35 +0800

    IOS AppDelegation function write into YHAppication

--- app/src/main/java/com/intfocus/yh_android/LoginActivity.java
+++ app/src/main/java/com/intfocus/yh_android/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.intfocus.yh_android;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -20,16 +19,10 @@ import com.intfocus.yh_android.util.ApiHelper;
 import com.intfocus.yh_android.util.FileUtil;
 import com.intfocus.yh_android.util.URLs;
 
-import org.OpenUDID.OpenUDID_manager;
-
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 import com.pgyersdk.update.PgyUpdateManager;
 
 public class LoginActivity extends BaseActivity {
@@ -50,40 +43,16 @@ public class LoginActivity extends BaseActivity {
                 return false;
             }
         });
-
         mWebView.addJavascriptInterface(new JavaScriptInterface(), "AndroidJSBridge");
 
+
+        String htmlPath = String.format("file:///%s/loading/login.html", FileUtil.sharedPath());
+        mWebView.loadUrl(htmlPath);
+
         /*
-         *  初始化OpenUDID, 设备唯一化
+         * 检测登录界面，版本是否升级
          */
-        OpenUDID_manager.sync(getApplicationContext());
-
-
-
-        try {
-            String htmlPath = String.format("file:///%s/loading/login.html", FileUtil.sharedPath());
-            mWebView.loadUrl(htmlPath);
-
-            /*
-             *  解压表态资源
-             */
-            File file = new File(String.format("%s/loading", FileUtil.sharedPath()));
-            if(!file.exists()) {
-                unZip("loading.zip", FileUtil.sharedPath(), true);
-            }
-            file = new File(String.format("%s/assets", FileUtil.sharedPath()));
-            if(!file.exists()) {
-                unZip("assets.zip", FileUtil.sharedPath(), true);
-            }
-
-            /*
-             * 检测登录界面，版本是否升级
-             */
-            checkVersionUpgrade(FileUtil.sharedPath());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        checkVersionUpgrade(FileUtil.sharedPath());
 
         /*
          *  加载服务器网页
@@ -179,57 +148,6 @@ public class LoginActivity extends BaseActivity {
         }
     };
 
-    /**
-     * 解压assets的zip压缩文件到指定目录
-     * @param context上下文对象
-     * @param assetName压缩文件名
-     * @param outputDirectory输出目录
-     * @param isReWrite是否覆盖
-     * @throws IOException
-     */
-    public void unZip(String assetName, String outputDirectory, boolean isReWrite) throws IOException {
-        // 创建解压目标目录
-        File file = new File(outputDirectory);
-        // 如果目标目录不存在，则创建
-        if (!file.exists()) {
-            file.mkdirs();
-        }
-        // 打开压缩文件
-        InputStream inputStream = getApplicationContext().getAssets().open(assetName);
-        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
-        // 读取一个进入点
-        ZipEntry zipEntry = zipInputStream.getNextEntry();
-        // 使用1Mbuffer
-        byte[] buffer = new byte[10*1024 * 1024];
-        // 解压时字节计数
-        int count = 0;
-        // 如果进入点为空说明已经遍历完所有压缩包中文件和目录
-        while (zipEntry != null) {
-            // 如果是一个目录
-            if (zipEntry.isDirectory()) {
-                file = new File(outputDirectory + File.separator + zipEntry.getName());
-                // 文件需要覆盖或者是文件不存在
-                if (isReWrite || !file.exists()) {
-                    file.mkdir();
-                }
-            } else {
-                // 如果是文件
-                file = new File(outputDirectory + File.separator + zipEntry.getName());
-                // 文件需要覆盖或者文件不存在，则解压文件
-                if (isReWrite || !file.exists()) {
-                    file.createNewFile();
-                    FileOutputStream fileOutputStream = new FileOutputStream(file);
-                    while ((count = zipInputStream.read(buffer)) > 0) {
-                        fileOutputStream.write(buffer, 0, count);
-                    }
-                    fileOutputStream.close();
-                }
-            }
-            // 定位到下一个文件入口
-            zipEntry = zipInputStream.getNextEntry();
-        }
-        zipInputStream.close();
-    }
 
     private class JavaScriptInterface {
         /*
---
+++ app/src/main/java/com/intfocus/yh_android/YHApplication.java
@@ -0,0 +1,118 @@
+package com.intfocus.yh_android;
+
+import com.intfocus.yh_android.util.FileUtil;
+import com.pgyersdk.crash.PgyCrashManager;
+import android.app.Application;
+import android.util.Log;
+
+import org.OpenUDID.OpenUDID_manager;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Created by lijunjie on 16/1/15.
+ */
+public class YHApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+
+        /*
+         *  蒲公英平台，收集闪退日志
+         */
+        PgyCrashManager.register(this);
+
+        /*
+         *  初始化OpenUDID, 设备唯一化
+         */
+        OpenUDID_manager.sync(getApplicationContext());
+
+        /*
+         *  解压表态资源
+         */
+        try {
+            File file = new File(String.format("%s/loading", FileUtil.sharedPath()));
+            if(!file.exists()) {
+                unZip("loading.zip", FileUtil.sharedPath(), true);
+                Log.i("Assets", "解压 loading.zip");
+            }
+            else {
+                Log.i("AssetsExist", file.getAbsolutePath());
+                //FileUtils.deleteDirectory(file);
+            }
+            file = new File(String.format("%s/assets", FileUtil.sharedPath()));
+            if(!file.exists()) {
+                unZip("assets.zip", FileUtil.sharedPath(), true);
+                Log.i("Assets", "解压 assets.zip");
+            }
+            else {
+                Log.i("AssetsExist", file.getAbsolutePath());
+                //FileUtils.deleteDirectory(file);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("App", "hello world");
+    }
+
+    /**
+     * 解压assets的zip压缩文件到指定目录
+     * @param context上下文对象
+     * @param assetName压缩文件名
+     * @param outputDirectory输出目录
+     * @param isReWrite是否覆盖
+     * @throws IOException
+     */
+    public void unZip(String assetName, String outputDirectory, boolean isReWrite) throws IOException {
+        // 创建解压目标目录
+        File file = new File(outputDirectory);
+        // 如果目标目录不存在，则创建
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        // 打开压缩文件
+        InputStream inputStream = getApplicationContext().getAssets().open(assetName);
+        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+        // 读取一个进入点
+        ZipEntry zipEntry = zipInputStream.getNextEntry();
+        // 使用1Mbuffer
+        byte[] buffer = new byte[10*1024 * 1024];
+        // 解压时字节计数
+        int count = 0;
+        // 如果进入点为空说明已经遍历完所有压缩包中文件和目录
+        while (zipEntry != null) {
+            // 如果是一个目录
+            if (zipEntry.isDirectory()) {
+                file = new File(outputDirectory + File.separator + zipEntry.getName());
+                // 文件需要覆盖或者是文件不存在
+                if (isReWrite || !file.exists()) {
+                    file.mkdir();
+                }
+            } else {
+                // 如果是文件
+                file = new File(outputDirectory + File.separator + zipEntry.getName());
+                // 文件需要覆盖或者文件不存在，则解压文件
+                if (isReWrite || !file.exists()) {
+                    file.createNewFile();
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    while ((count = zipInputStream.read(buffer)) > 0) {
+                        fileOutputStream.write(buffer, 0, count);
+                    }
+                    fileOutputStream.close();
+                }
+            }
+            // 定位到下一个文件入口
+            zipEntry = zipInputStream.getNextEntry();
+        }
+        zipInputStream.close();
+    }
+}
