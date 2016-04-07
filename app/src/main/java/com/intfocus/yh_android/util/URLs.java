64cd2396fdef30a1f534a249d2665638d9aa7986
jay16 <jay_li@intfocus.com>
2016-04-07 10:18:53 +0800

    branch with shengyiplus

--- app/src/main/java/com/intfocus/yh_android/util/URLs.java
+++ app/src/main/java/com/intfocus/yh_android/util/URLs.java
@@ -19,6 +19,7 @@ import java.util.Date;
 public class URLs implements Serializable {
 
     public final static String HOST = "http://yonghui.idata.mobi";
+    public final static String HOST2  = "http://121.199.38.185:4567";
     public final static String HOST1 = "http://10.0.3.2:4567";
     public final static String HOST3 = "http://www.google.com";
 
